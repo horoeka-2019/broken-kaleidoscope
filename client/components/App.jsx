f861f0e20ee444b00efada58ab581bc8f94e2ff3
Steve Yoo <steve.yoo@hotmail.com>
2019-11-05 11:41:05 +1300

    Added pixel color and created multiple pixels in app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Pixel from './Pixel'
 
+let array = []
+for (let i = 0; i < 100; i++) {
+  array.push('')
+}
+
 const App = () => {
   return (
     <React.Fragment>
       <div>React development has begun!</div>
-      <Pixel />
+      {array.map((ele, i) => (
+        <Pixel key={i}/>
+      ))}
     </React.Fragment>
   )
 }
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 class Pixel extends React.Component {
   render () {
     return (
-      <div style={{ fontFamily: 'Time New Roman', height: '50px', width: '50px', backgroundColor: '#FF0000' }}></div>
+      <div style={{ fontFamily: 'Time New Roman', height: '50px', width: '50px', backgroundColor: 'blue' }}></div>
 
     )
   }
