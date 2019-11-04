b2e4cee482a3a671449af4960b9583dbf52b611a
Andria Cristia Hibe <andriacohibe@gmail.com>
2019-11-05 11:11:21 +1300

    Change state style to come up with random background color

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 class Pixel extends React.Component {
+  randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
   state = {
     style: {
       height: '100px',
       width: '100px',
-      backgroundColor: 'cornflowerblue'
+      backgroundColor: this.randomHexColor()
     }
   }
+
   render () {
     return (
       <React.Fragment>
