2dfc9c7551c7d1313aba8f92dc3c76c0cad2c44e
Jill Glass <glass.nz@gmail.com>
2019-11-05 10:42:13 +1300

    Pixel.jsx, App.jsx set up

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div></div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Pixel
