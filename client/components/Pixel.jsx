3264cec04f0ce2e5da1c000284a90e2f31800184
Steve Yoo <steve.yoo@hotmail.com>
2019-11-05 11:10:16 +1300

    inline css update

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 class Pixel extends React.Component {
   render () {
     return (
-      <div style={{ fontFamily: 'Time New Roman' }}></div>
+      <div style={{ fontFamily: 'Time New Roman', height: '50px', width: '50px', backgroundColor: '#FF0000' }}></div>
+
     )
   }
 }
