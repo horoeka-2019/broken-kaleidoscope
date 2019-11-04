0d0955aae67dd210c28e9a8c6db3fe02c1af0d91
Samuel Hoe Yan Looi <samhylooi@gmail.com>
2019-11-05 10:49:11 +1300

    Created Pixel.jsx and exported it to App.jsx

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  render () {
+    return (
+      <div style={{ fontFamily: 'Time New Roman' }}></div>
+    )
+  }
+}
+
+export default Pixel
