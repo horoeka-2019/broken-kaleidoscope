7b8dc73de2e6df09ccbc2693e3f50cefcd86a217
Yingli Niu <sunnyyingli@gmail.com>
2019-11-05 11:36:55 +1300

    can change color when clicking it

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -10,9 +10,16 @@ class Pixel extends React.Component {
     this.state = { style: { height: '60px', width: '60px', backgroundColor: randomHexColor() } }
   }
 
+  changeColor = (event) => {
+    event.preventDefault()
+    this.setState({
+      style: { height: '60px', width: '60px', backgroundColor: randomHexColor() }
+    })
+  }
+
   render () {
     return (
-      <div style={this.state.style}>
+      <div style={this.state.style} onClick={this.changeColor}>
       </div>
     )
   }
