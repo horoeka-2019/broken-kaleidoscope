95e39051aafe038bab991cafb169cbfc3353f8cd
Marina Tateoka <marinaslussarek@gmail.com>
2019-11-05 11:35:20 +1300

    create Pixel component and add some style

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div style={{
+          height: '50px',
+          width: '50px',
+          backgroundColor: 'cornflowerblue'
+        }}>  </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Pixel
