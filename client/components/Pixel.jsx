75a47940f74cddf9f224440d7012c36982a3543f
Andria Cristia Hibe <andriacohibe@gmail.com>
2019-11-05 11:04:19 +1300

    Turn style attribute into component state

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 class Pixel extends React.Component {
+  state = {
+    style: {
+      height: '100px',
+      width: '100px',
+      backgroundColor: 'cornflowerblue'
+    }
+  }
   render () {
     return (
       <React.Fragment>
-        <div style={{
-          fontFamily: 'Times New Roman',
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'cornflowerblue'
-        
-        }}></div>
+        <div style={this.state.style}>
+        </div>
       </React.Fragment>
     )
   }
