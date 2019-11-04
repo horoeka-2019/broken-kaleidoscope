5fd95ac85b3b22ff4d966d61d754b9734c7eeb77
Jackie Shuyu <jackie.shuyu@gmail.com>
2019-11-05 11:30:02 +1300

    create function to get random color in style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
 import Pixel from './Pixel'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { style: { height: '60px', width: '60px', backgroundColor: 'greenyellow' } }
-  }
-
-  handleChange = (event) => {
-    const newColor = event.target.value
-    this.setState({
-      style: { height: '60px', width: '60px', backgroundColor: newColor }
-    })
-  }
-  render () {
-    return (
-      <div >React development has begun!
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <Pixel style={this.state.style}/>
-        <input type='text' name='changeColor' onChange={this.handleChange}></input>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div >React development has begun!
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+    </div>
+  )
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const Pixel = (props) => {
-  return (
-    <div style={props.style}>
-    </div>
-  )
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+class Pixel extends React.Component {
+  constructor (props) {
+    super(props)
+    // this.randomHexColor()
+    this.state = { style: { height: '60px', width: '60px', backgroundColor: randomHexColor() } }
+  }
+
+  render () {
+    return (
+      <div style={this.state.style}>
+      </div>
+    )
+  }
 }
 
 export default Pixel
