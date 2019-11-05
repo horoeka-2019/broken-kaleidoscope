0349870cb5e0609294f1fa58f79a2b5bab943916
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-11-05 17:03:59 +1300

    constructor created for Pixel

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 
 class Pixel extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      style: {
+        height: '50px',
+        width: '50px',
+        backgroundColor: 'red'
+      }
+
+    }
+  }
+
   render () {
     return (
       <React.Fragment>
-        <div style= {{
-          height: '50px',
-          width: '50px',
-          backgroundColor: 'red'
-        }}></div>
+        <div style={ this.state.style }>
+
+        </div>
+
       </React.Fragment>
 
     )
   }
 }
-
 export default Pixel
