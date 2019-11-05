2cc7e7b01117430f9b9a3f0e21eba2b959e108b7
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-11-05 16:10:02 +1300

    Pixel component created

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div></div>
+      </React.Fragment>
+
+    )
+  }
+}
+
+export default Pixel
